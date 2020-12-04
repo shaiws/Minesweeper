ceaebf4738db22a35873c978b6b2cb67bf1bbbbe
shaiws <shaiws2@gmail.com>
2020-12-04 12:36:36 +0200

    Organized.

---
+++ project/Minesweeper.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#define COLUMNS 8
+#define ROWS 8
+
+/*Functions decleration*/
+int chooseBoardSize();
+void printBoard(char board[ROWS][COLUMNS]);
+
+void main(){
+    bool win = false;
+    int amountOfMines;
+    while(!win){
+        win = true;
+        printf("test");
+    }
+    
+}
